ecc6a68522e99dc0e020c41908bea7b919d39e3f
AdrianTofan <adrianvasile.tofan@gmail.com>
2019-11-22 01:31:51 +0000

    Added a scrolling background + gameover UI + death when touching crates/rocks.

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -5f)
+            SceneManager.LoadScene("gameOver");
+    }
+}
